c674029847de76e58222b7ed40c5cc4da1342d74
habibafathalla <habiba.elshenofy01@eng-st.cu.edu.eg>
2022-10-22 20:05:18 +0200

    GUI_Repairs

--- Demo1.py
+++ Demo1.py
@@ -3,6 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import sys
+
+from traitlets import default
 from logic import logic
 # from streamlit_option_menu import option_menu
 from scipy.misc import electrocardiogram
@@ -68,9 +70,12 @@ with st.sidebar:
     Signals = pd.DataFrame(get_data())
     st.write(Signals)
 
-    Deleted_Signal = st.number_input(
+    generate_expander_Delete = st.sidebar.expander(
+        "Delete a Signal ", expanded=False)
+
+    Deleted_Signal = generate_expander_Delete.number_input(
         "Enter the row of the Signal", step=1)
-    delete_button = st.button('Delete A signal')
+    delete_button = generate_expander_Delete.button('Delete A signal')
     if delete_button:
         if Deleted_Signal > len(Signals)-1 or Deleted_Signal < 0:
             st.text("Invalid number")
@@ -84,16 +89,22 @@ with st.sidebar:
                 st.session_state.sum = logic.sum_signals()
 
     st.write("")
-
-    if st.button(" Upload a Signal"):
-        file = st.file_uploader("Upload Your Signal")
+    generate_expander_Upload = st.sidebar.expander(
+        "Upload Signal", expanded=False)
+    file = generate_expander_Upload.file_uploader("")
+    if generate_expander_Upload.button(" Upload a Signal"):
         Data = pd.read_csv(file)
 
     if st.checkbox("Add Noise"):
+
         snr = st.slider("SNR", 0, 100, step=1)
         noised_signal = logic.add_noise(st.session_state.sum, snr)
 
-    st.button("Save")
+    generate_expander_save = st.sidebar.expander(
+        "Save Signal", expanded=False)
+    Folder_Name = generate_expander_save.text_input("Folder Name")
+    Save_button_clicked = generate_expander_save.button("Save")
+
 
 sample_rate = st.slider("Sample Rate", 1, 5, step=1)
 # print(sample_rate)
