aefdaaa0097b8ab89e853100600f77792b6242a8
habibafathalla <habiba.elshenofy01@eng-st.cu.edu.eg>
2022-10-22 18:56:33 +0200

    Min_value_added

--- Demo1.py
+++ Demo1.py
@@ -44,7 +44,8 @@ st.title(" Welcome to Sampling Studio")
 def get_data():
     return []
 
-fig= plt.figure()
+
+fig = plt.figure()
 
 with st.sidebar:
 
@@ -52,12 +53,14 @@ with st.sidebar:
     generate_expander = st.sidebar.expander(
         "Generate/Add Signal ", expanded=False)
     Label = generate_expander.text_input("Label")
-    freq = generate_expander.number_input("Frecquency in Hz", step = 1)
-    Amplitude = generate_expander.number_input("Amplitude", step = 1)
+    freq = generate_expander.number_input(
+        "Frecquency in Hz", step=1, min_value=1)
+    Amplitude = generate_expander.number_input(
+        "Amplitude", step=1, min_value=1)
     Add_button_clicked = generate_expander.button("Save", key=1)
 
     if Add_button_clicked:
-        logic.add_signals(Amplitude, freq) 
+        logic.add_signals(Amplitude, freq)
         st.session_state.sum = logic.sum_signals()
 
         get_data().append(
@@ -66,38 +69,41 @@ with st.sidebar:
     st.write(Signals)
 
     Deleted_Signal = st.number_input(
-            "Enter the row of the Signal", step = 1)
+        "Enter the row of the Signal", step=1)
     delete_button = st.button('Delete A signal')
     if delete_button:
-        if Deleted_Signal > len(Signals)-1 or Deleted_Signal < 0  :
+        if Deleted_Signal > len(Signals)-1 or Deleted_Signal < 0:
             st.text("Invalid number")
-        else :
-            logic.remove_Signal(int(Deleted_Signal),get_data())
+        else:
+            logic.remove_Signal(int(Deleted_Signal), get_data())
             Signals.drop([Deleted_Signal], axis=0, inplace=True)
-    
-            st.session_state.sum = logic.sum_signals() 
+
+            if (len(Signals) == 0):
+                st.session_state.sum = 0
+            else:
+                st.session_state.sum = logic.sum_signals()
 
     st.write("")
 
     if st.button(" Upload a Signal"):
         file = st.file_uploader("Upload Your Signal")
         Data = pd.read_csv(file)
-    
-    if st.checkbox("Add Noise") :
-        snr = st.slider("SNR",0, 100, step= 1)
+
+    if st.checkbox("Add Noise"):
+        snr = st.slider("SNR", 0, 100, step=1)
         noised_signal = logic.add_noise(st.session_state.sum, snr)
 
     st.button("Save")
 
-sample_rate = st.slider("Sample Rate", 1, 5, step= 1)
+sample_rate = st.slider("Sample Rate", 1, 5, step=1)
 # print(sample_rate)
 maxF = logic.get_maxF()
 # y = logic.sinc_Interpolation(sample_rate,maxF)
 # for i in range(0, len(st.session_state.sinW)):
-if type(st.session_state.sum) is  np.ndarray:
+if type(st.session_state.sum) is np.ndarray:
     y_new = logic.sinc_Interpolation(sample_rate*logic.get_maxF())
     plt.subplot(211)
-    plt.plot(logic.time,y_new)
+    plt.plot(logic.time, y_new)
     plt.subplot(212)
-    plt.plot(logic.time,st.session_state.sum)
+    plt.plot(logic.time, st.session_state.sum)
 st.pyplot()
